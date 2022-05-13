a4c76e53618da1c65bd65f20e2db3ab0d6c49e4d
Leda <ledathemis@protonmail.com>
2022-05-13 19:52:58 +0000

    add tests for WonDisplay

---
+++ src/__tests__/WonDisplay.test.js
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import WonDisplay from '../components/WonDisplay';
+import { getHumanReadableTime } from '../helpers/utils';
+import userEvent from '@testing-library/user-event';
+
+test('should render time with text', () => {
+  const seconds = 1240;
+  render(<WonDisplay seconds={seconds} />);
+
+  expect(screen.getByText(`You won, with a time of ${getHumanReadableTime(seconds)}!`)).toBeVisible();
+});
+
+describe('test should reload page on click', () => {
+  const original = window.location;
+
+  beforeAll(() => {
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: { reload: jest.fn() },
+    });
+  });
+
+  afterAll(() => {
+    Object.defineProperty(window, 'location', { configurable: true, value: original });
+  });
+
+  it('calls window.location.reload if clicked', () => {
+    render(<WonDisplay seconds={0} />);
+
+    const refreshPageButton = screen.getByText('Refresh Page');
+
+    userEvent.click(refreshPageButton);
+
+    expect(window.location.reload).toHaveBeenCalled();
+  });
+
+  it('does not call window.location.reload if not clicked', () => {
+    render(<WonDisplay seconds={0} />);
+
+    expect(window.location.reload).not.toHaveBeenCalled();
+  });
+});

dc4e6d6ccc039b681c9ce1ffdef35309d3acf8cf
Leda <ledathemis@protonmail.com>
2022-05-13 16:08:36 +0000

    handle case when player wins

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import Header from './components/Header';
 import Status from './components/Status';
 import SelectionPopup from './components/SelectionPopup';
 import Marker from './components/Marker';
+import Timer from './components/Timer';
+import Overlay from './components/Overlay';
+import WonDisplay from './components/WonDisplay';
 
 import whereWaldoImage from './assets/where-waldo-1.jpeg';
 
 import { fetchPositionsFromDB } from './helpers/db';
-import { checkSelection, getSelection } from './helpers/utils';
-import Timer from './components/Timer';
+import { checkSelection, getSelection, getHumanReadableTime } from './helpers/utils';
 
 const App = () => {
   const [currentCoordinates, setCurrentCoordinates] = useState({ x: 0, y: 0 });
@@ -27,9 +29,12 @@ const App = () => {
   const [isDisplayingStatus, setIsDisplayingStatus] = useState(false);
   const [statusText, setStatusText] = useState('');
 
-  const [isStarted, setIsStarted] = useState(false);
-
   const [seconds, setSeconds] = useState(0);
+
+  const [isStarted, setIsStarted] = useState(false);
+  const [isWon, setIsWon] = useState(false);
+
+  const [showOverlay, setShowOverlay] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -38,6 +43,13 @@ const App = () => {
       setAvailableSelections(fetchedPositions.filter((pos) => pos.id !== 'props'));
     })();
   }, []);
+
+  useEffect(() => {
+    console.log(availableSelections);
+    if (checkIfWon(availableSelections)) {
+      handleWin();
+    }
+  }, [availableSelections]);
 
   const markSelectionCorrect = (correctSelection, mouseX, mouseY) => {
     setCorrectSelections(correctSelections.concat({ mouseX, mouseY, id: correctSelection }));
@@ -64,13 +76,25 @@ const App = () => {
     }
   };
 
+  const checkIfWon = (availableSelections) => {
+    return availableSelections.length === 9;
+  };
+
+  const handleWin = () => {
+    setIsPopupActive(false);
+    setIsWon(true);
+    setShowOverlay(true);
+  };
+
   return (
     <div className="App">
       {isDisplayingStatus && <Status text={statusText} />}
+      {isWon && <WonDisplay seconds={seconds} />}
+      {showOverlay && <Overlay />}
 
       <div id="header-with-timer">
         <Header isStarted={isStarted} />
-        <Timer seconds={seconds} setSeconds={setSeconds} isStarted={isStarted} />
+        {!isWon && <Timer seconds={seconds} setSeconds={setSeconds} isStarted={isStarted} />}
       </div>
 
       {isPopupActive && (
@@ -86,9 +110,7 @@ const App = () => {
 
       {isPopupActive && <Marker x={mouseCoordinates.x} y={mouseCoordinates.y} />}
 
-      {correctSelections.map(({ id, mouseX, mouseY }) => (
-        <Marker key={id} x={mouseX} y={mouseY} />
-      ))}
+      {!isWon && correctSelections.map(({ id, mouseX, mouseY }) => <Marker key={id} x={mouseX} y={mouseY} />)}
 
       <Display
         imageObject={whereWaldoImage}

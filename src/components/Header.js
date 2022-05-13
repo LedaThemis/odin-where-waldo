8e337aecd049af6ead329400a11037537ae118d8
Leda <ledathemis@protonmail.com>
2022-05-13 08:42:38 +0000

    add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <header id="header">
+      <h1 id="header--title">Where's Waldo?</h1>
+    </header>
+  );
+};
+
+export default Header;

edfb8205e45cbc5782c2cda4d9b95c7a29c03ad5
Leda <ledathemis@protonmail.com>
2022-05-13 12:59:45 +0000

    add selection popup

---
+++ src/components/SelectionPopup.js
@@ -0,0 +1,32 @@
+import '../styles/SelectionPopup.css';
+
+const SelectionPopup = ({ x, y, availableSelections, currentSelection, setCurrentSelection, submitSelection }) => {
+  const getOption = (name, value) => {
+    return (
+      <option key={`${value}-key`} value={value}>
+        {name}
+      </option>
+    );
+  };
+  const getAvailableOptions = (options) => {
+    return options.map((option) => getOption(option.name, option.id));
+  };
+
+  return (
+    <div id="selection--popup" style={{ top: y, left: x }}>
+      <p id="selection--popup--title">Select what you found</p>
+      <select
+        id="selection--popup--select"
+        value={currentSelection}
+        onChange={(e) => setCurrentSelection(e.target.value)}
+      >
+        {getAvailableOptions(availableSelections)}
+      </select>
+      <button id="selection--popup--submit" type="button" onClick={submitSelection}>
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default SelectionPopup;

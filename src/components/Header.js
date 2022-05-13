4298d833a0039d858d93a8e0776f5e4ac5e9fd3b
Leda <ledathemis@protonmail.com>
2022-05-13 14:41:42 +0000

    add notice to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <header id="header">
       <h1 id="header--title">Where's Waldo?</h1>
+      <p id="header--notice">Click Anywhere!</p>
     </header>
   );
 };

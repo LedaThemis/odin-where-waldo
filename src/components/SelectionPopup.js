360438ffb48b7e46a6ac69d5aec291464e129c96
Leda <ledathemis@protonmail.com>
2022-05-13 20:08:18 +0000

    add testid for testing

--- src/components/SelectionPopup.js
+++ src/components/SelectionPopup.js
@@ -13,7 +13,7 @@ const SelectionPopup = ({ x, y, availableSelections, currentSelection, setCurren
   };
 
   return (
-    <div id="selection--popup" style={{ top: y, left: x }}>
+    <div id="selection--popup" data-testid="selection--popup--div" style={{ top: y, left: x }}>
       <p id="selection--popup--title">Select what you found</p>
       <select
         id="selection--popup--select"

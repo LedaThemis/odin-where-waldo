300d94abd4cb51c1ae37a01eec3e485bdd42a16e
Leda <ledathemis@protonmail.com>
2022-05-13 14:04:58 +0000

    add Marker component

---
+++ src/components/Marker.js
@@ -0,0 +1,12 @@
+import { FaMapMarkerAlt } from 'react-icons/fa';
+import '../styles/Marker.css';
+
+const Marker = ({ x, y }) => {
+  return (
+    <div className="marker" style={{ left: x - 25, top: y - 50 }}>
+      <FaMapMarkerAlt size={50} color={'rgb(0,255,0,0.75)'} />
+    </div>
+  );
+};
+
+export default Marker;

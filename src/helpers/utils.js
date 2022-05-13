3581b0b3d18322b923c7618890acf712436a34d9
Leda <ledathemis@protonmail.com>
2022-05-13 12:57:11 +0000

    add functionality to check if selection is correct

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -5,4 +5,48 @@ const convertPositionToPercentage = (x, y, width, height) => {
   return { percentageX, percentageY };
 };
 
-export { convertPositionToPercentage };
+const getBorderCoordinates = (x, y, width, height) => {
+  return { x1: x, x2: x + width, y1: y, y2: y + height };
+};
+
+const getSelection = (positionsData, id) => {
+  return positionsData.find((selection) => selection.id === id);
+};
+
+const getProps = (positionsData) => {
+  return positionsData.find((e) => e.id === 'props');
+};
+
+const withinRange = (start, end, value) => value >= start && value <= end;
+
+const getCorrectBorderPositions = (positionsData, selection) => {
+  const { imageWidth, imageHeight } = getProps(positionsData);
+  const { x, y, width, height } = selection;
+  const { x1, x2, y1, y2 } = getBorderCoordinates(x, y, width, height);
+  const leftBorder = convertPositionToPercentage(x1, y1, imageWidth, imageHeight);
+  const rightBorder = convertPositionToPercentage(x2, y2, imageWidth, imageHeight);
+  const percentageX1 = leftBorder.percentageX;
+  const percentageY1 = leftBorder.percentageY;
+  const percentageX2 = rightBorder.percentageX;
+  const percentageY2 = rightBorder.percentageY;
+
+  return { percentageX1, percentageX2, percentageY1, percentageY2 };
+};
+
+const checkSelection = (positionsData, currentSelection, currentCoordinates) => {
+  const selection = getSelection(positionsData, currentSelection);
+  const currentPercentageX = currentCoordinates.x;
+  const currentPercentageY = currentCoordinates.y;
+
+  const { percentageX1, percentageX2, percentageY1, percentageY2 } = getCorrectBorderPositions(
+    positionsData,
+    selection
+  );
+
+  return (
+    withinRange(percentageX1, percentageX2, currentPercentageX) &&
+    withinRange(percentageY1, percentageY2, currentPercentageY)
+  );
+};
+
+export { convertPositionToPercentage, getSelection, checkSelection };

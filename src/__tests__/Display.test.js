b6ed66f0b93eaa6fcd4252e5f3278646372f1461
Leda <ledathemis@protonmail.com>
2022-05-14 16:42:24 +0000

    add test for calling handleImageClick on image click

--- src/__tests__/Display.test.js
+++ src/__tests__/Display.test.js
@@ -1,10 +1,34 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import Display from '../components/Display';
+
+import * as handlers from '../helpers/handlers';
 
 test('should render image with provided source', () => {
   const imgSrc = 'sample/src';
   render(<Display imageObject={imgSrc} />);
 
   expect(screen.getByRole('img')).toHaveAttribute('src', imgSrc);
+});
+
+test('should call handleImageClick on click', () => {
+  const spyHandleImageClick = jest.spyOn(handlers, 'handleImageClick');
+
+  render(
+    <Display
+      imageObject={'https://via.placeholder.com/1/1'}
+      isStarted={true}
+      setIsStarted={() => {}}
+      setCurrentCoordinates={() => {}}
+      setMouseCoordinates={() => {}}
+      setIsPopupActive={() => {}}
+    />
+  );
+
+  expect(spyHandleImageClick).not.toHaveBeenCalled();
+
+  userEvent.click(screen.getByRole('img'));
+
+  expect(spyHandleImageClick).toHaveBeenCalled();
 });

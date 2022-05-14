16f046ecca6dd8f1e78a1348eac4ccc487065aa7
Leda <ledathemis@protonmail.com>
2022-05-14 17:30:12 +0000

    add tests for getSelection

--- src/helpers/__tests__/utils.test.js
+++ src/helpers/__tests__/utils.test.js
@@ -1,4 +1,4 @@
-import { convertPositionToPercentage } from '../utils';
+import { convertPositionToPercentage, getSelection } from '../utils';
 
 describe('test convertPositionToPercentage', () => {
   const result = convertPositionToPercentage(10, 20, 22.5, 45);
@@ -8,5 +8,20 @@ describe('test convertPositionToPercentage', () => {
   });
   it('returns given position as percentage with 2 decimals precision', () => {
     expect(result).toMatchObject({ percentageX: 44.44, percentageY: 44.44 });
+  });
+});
+
+describe('test getSelection', () => {
+  it('should return undefined if selection does not exist', () => {
+    const result = getSelection([{ id: 0 }, { id: 1 }], 2);
+
+    expect(result).toBeUndefined();
+  });
+
+  it('should return correct selection if exists', () => {
+    const correctSelection = { id: 1 };
+    const result = getSelection([{ id: 0 }, correctSelection], correctSelection.id);
+
+    expect(result).toMatchObject(correctSelection);
   });
 });

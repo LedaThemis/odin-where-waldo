0e4fa03fdeeb7a85a2b391c003ddab19c3a4d5e0
Leda <ledathemis@protonmail.com>
2022-05-14 17:23:46 +0000

    add test to check for correct number of positions

--- src/helpers/__tests__/utils.test.js
+++ src/helpers/__tests__/utils.test.js
@@ -1,8 +1,12 @@
 import { convertPositionToPercentage } from '../utils';
 
 describe('test convertPositionToPercentage', () => {
+  const result = convertPositionToPercentage(10, 20, 22.5, 45);
+
+  it('returns correct number of positions', () => {
+    expect(Object.keys(result).length).toBe(2);
+  });
   it('returns given position as percentage with 2 decimals precision', () => {
-    const result = convertPositionToPercentage(10, 20, 22.5, 45);
     expect(result).toMatchObject({ percentageX: 44.44, percentageY: 44.44 });
   });
 });

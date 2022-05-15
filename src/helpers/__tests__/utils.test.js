a314ab8f8174cc6e8e889892af23f99e2a5df968
Leda <ledathemis@protonmail.com>
2022-05-15 17:29:46 +0000

    add tests for checkIfWon

--- src/helpers/__tests__/utils.test.js
+++ src/helpers/__tests__/utils.test.js
@@ -22,45 +22,13 @@ const {
   getOption,
 } = exportedForTesting;
 
-describe('getSelection', () => {
-  it('should return undefined if selection does not exist', () => {
-    const result = getSelection([{ id: 0 }, { id: 1 }], 2);
-
-    expect(result).toBeUndefined();
+describe('checkIfWon', () => {
+  it('should return true if availableSelections is empty', () => {
+    expect(checkIfWon([])).toBe(true);
   });
 
-  it('should return correct selection if exists', () => {
-    const correctSelection = { id: 1 };
-    const result = getSelection([{ id: 0 }, correctSelection], correctSelection.id);
-
-    expect(result).toMatchObject(correctSelection);
-  });
-});
-
-describe('checkSelection', () => {
-  it('should return true if provided coordinates is within correct range', () => {
-    const positionsData = [
-      { id: 1, x: 0, y: 0, width: 5, height: 5 },
-      { id: 'props', imageWidth: 10, imageHeight: 10 },
-    ];
-    const currentSelection = 1;
-    const currentCoordinates = { x: 50, y: 50 };
-
-    const result = checkSelection(positionsData, currentSelection, currentCoordinates);
-
-    expect(result).toBe(true);
-  });
-  it('should return false if provided coordinates is not within correct range', () => {
-    const positionsData = [
-      { id: 1, x: 0, y: 0, width: 6, height: 6 },
-      { id: 'props', imageWidth: 30, imageHeight: 30 },
-    ];
-    const currentSelection = 1;
-    const currentCoordinates = { x: 50, y: 50 };
-
-    const result = checkSelection(positionsData, currentSelection, currentCoordinates);
-
-    expect(result).toBe(false);
+  it('should return false if availableSelections is not empty', () => {
+    expect(checkIfWon(['not', 'empty'])).toBe(false);
   });
 });
 
@@ -111,6 +79,48 @@ describe('startTimer', () => {
 
     expect(setSeconds).toHaveBeenCalledTimes(2);
     expect(setSeconds).toHaveBeenCalledWith(expect.any(Function));
+  });
+});
+
+describe('getSelection', () => {
+  it('should return undefined if selection does not exist', () => {
+    const result = getSelection([{ id: 0 }, { id: 1 }], 2);
+
+    expect(result).toBeUndefined();
+  });
+
+  it('should return correct selection if exists', () => {
+    const correctSelection = { id: 1 };
+    const result = getSelection([{ id: 0 }, correctSelection], correctSelection.id);
+
+    expect(result).toMatchObject(correctSelection);
+  });
+});
+
+describe('checkSelection', () => {
+  it('should return true if provided coordinates is within correct range', () => {
+    const positionsData = [
+      { id: 1, x: 0, y: 0, width: 5, height: 5 },
+      { id: 'props', imageWidth: 10, imageHeight: 10 },
+    ];
+    const currentSelection = 1;
+    const currentCoordinates = { x: 50, y: 50 };
+
+    const result = checkSelection(positionsData, currentSelection, currentCoordinates);
+
+    expect(result).toBe(true);
+  });
+  it('should return false if provided coordinates is not within correct range', () => {
+    const positionsData = [
+      { id: 1, x: 0, y: 0, width: 6, height: 6 },
+      { id: 'props', imageWidth: 30, imageHeight: 30 },
+    ];
+    const currentSelection = 1;
+    const currentCoordinates = { x: 50, y: 50 };
+
+    const result = checkSelection(positionsData, currentSelection, currentCoordinates);
+
+    expect(result).toBe(false);
   });
 });
 
